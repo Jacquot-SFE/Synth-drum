dd4a64db9259903b059a309e2d4ed488a6d249f0
Jacquot-SFE <byron.jacquot@sparkfun.com>
2015-11-19 15:42:32 -0700

    Adding bandpass filter on noise
    I think it might exceed the Fc/4 limitation at very high modulation
    index and lo Q.

--- teensy-based/drumheart/drumheart.ino
+++ teensy-based/drumheart/drumheart.ino
@@ -10,12 +10,18 @@
 AudioSynthNoiseWhite     noise1;         //xy=573,86
 AudioSynthDrumHeart      drum1;
 
+AudioFilterStateVariable filter1;
+
 AudioMixer4              mixer;
 AudioEffectMultiply      mult1;
 AudioOutputI2S           i2s1;           //xy=968,448
 
 AudioConnection          patchCord1(drum1, 0, mixer, 0);
-AudioConnection          patchCord11(noise1, 0, mixer, 1);
+
+
+AudioConnection          patchCord11(noise1, 0, filter1, 0);
+AudioConnection          patchCord12(drum1, 1, filter1, 1);
+AudioConnection          patchCord13(filter1, 1, mixer, 1);
 
 AudioConnection          patchCord2(mixer, 0, mult1, 0);
 AudioConnection          patchCord22(drum1, 1, mult1, 1);
@@ -38,13 +44,15 @@ void paramUpdate()
 {
   uint16_t len, bend, pitch;
 
-  uint16_t mixadc;
+  uint16_t mixadc, q, filtmod;
   float    mix;
 
   len = analogRead(A1);
   bend = analogRead(A2);
   pitch = analogRead(A0);
-  mixadc = analogRead(A7);
+  mixadc = analogRead(A6);
+  q = analogRead(A7);
+  filtmod = analogRead(A3);
 
 #if 0
   Serial.print("Analog: ");
@@ -58,6 +66,11 @@ void paramUpdate()
   drum1.length((len * 2) + 50);
   drum1.pitchMod(bend);
   drum1.frequency(30 + (pitch>>1));
+  
+  filter1.frequency(30 + (pitch>>1));
+  filter1.resonance(0.7 + (((float)q * 4.3)/ 1024.0));
+
+  filter1.octaveControl( ((float)filtmod * 7.0) /1024.0);
 
   // 0 is osc, 1 is noise
   mix = (float)mixadc / 1024.0;
@@ -93,7 +106,10 @@ void setup() {
   noise1.amplitude(0.5);
   //sine1.frequency(1);
   //sine1.amplitude(1.0);
-  
+
+  filter1.frequency(100);
+  filter1.resonance(4.0);
+  filter1.octaveControl(3.0);  
 
   AudioInterrupts();
 
