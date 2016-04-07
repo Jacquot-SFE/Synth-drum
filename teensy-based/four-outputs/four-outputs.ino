766965da062e294fe99994b43ed2e94c86a229b1
Jacquot-SFE <byron.jacquot@sparkfun.com>
2016-04-06 19:01:17 -0600

    24-bits of I/O tested.
    three Boom-Jr boards in daisychain are working!

--- teensy-based/four-outputs/four-outputs.ino
+++ teensy-based/four-outputs/four-outputs.ino
@@ -4,68 +4,53 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
-#include "Synth-DrumHeart.h"
+#include "synth_simple_drum.h"
 
 // GUItool: begin automatically generated code
-AudioSynthDrumHeart      drum1;
-AudioEffectMultiply      mult1;
-
-AudioSynthDrumHeart      drum2;
-AudioEffectMultiply      mult2;
-
-AudioSynthDrumHeart      drum3;
-AudioEffectMultiply      mult3;
-
-AudioSynthDrumHeart      drum4;
-AudioEffectMultiply      mult4;
+//AudioSynthSimpleDrum     drum1;
+//AudioSynthSimpleDrum     drum2;
+//AudioSynthSimpleDrum     drum3;
+//AudioSynthSimpleDrum     drum4;
 
 AudioOutputI2SQuad       i2s1;           //xy=968,448
+AudioInputI2SQuad        in1;
 
-
-AudioConnection          patchCord10(drum1, 0, mult1, 0);
-AudioConnection          patchCord11(drum1, 1, mult1, 1);
-
-AudioConnection          patchCord20(drum2, 0, mult2, 0);
-AudioConnection          patchCord21(drum2, 1, mult2, 1);
-
-AudioConnection          patchCord30(drum3, 0, mult3, 0);
-AudioConnection          patchCord31(drum3, 1, mult3, 1);
-
-AudioConnection          patchCord40(drum4, 0, mult4, 0);
-AudioConnection          patchCord41(drum4, 1, mult4, 1);
-
-AudioConnection          patchCord50(mult1, 0, i2s1, 0);
-AudioConnection          patchCord60(mult2, 0, i2s1, 1);
-AudioConnection          patchCord70(mult3, 0, i2s1, 2);
-AudioConnection          patchCord80(mult4, 0, i2s1, 3);
+//AudioConnection          patchCord50(drum1, 0, i2s1, 0);
+//AudioConnection          patchCord60(drum2, 0, i2s1, 1);
+//AudioConnection          patchCord70(drum3, 0, i2s1, 2);
+//AudioConnection          patchCord80(drum4, 0, i2s1, 3);
+AudioConnection          patchCord50(in1, 0, i2s1, 0);
+AudioConnection          patchCord60(in1, 0, i2s1, 1);
+AudioConnection          patchCord70(in1, 0, i2s1, 2);
+AudioConnection          patchCord80(in1, 0, i2s1, 3);
 
 AudioControlSGTL5000     sgtl5000_1;     //xy=906,517
 AudioControlSGTL5000     sgtl5000_2;     //xy=906,517
 // GUItool: end automatically generated code
 
-
+#if 0
 void trigger(uint32_t channel)
 {
- 
+
   AudioNoInterrupts();
-  switch(channel & 0x03)
+  switch (channel & 0x03)
   {
-  case 0:
-    drum1.noteOn();
-    break;
-  case 1:
-    drum2.noteOn();
-    break;
-  case 2:
-    drum3.noteOn();
-    break;
-  case 3:
-    drum4.noteOn();
-    break;
+    case 0:
+      drum1.noteOn();
+      break;
+    case 1:
+      drum2.noteOn();
+      break;
+    case 2:
+      drum3.noteOn();
+      break;
+    case 3:
+      drum4.noteOn();
+      break;
   }
   AudioInterrupts();
 }
-
+#endif
 
 static uint32_t next;
 static uint32_t count;
@@ -84,24 +69,37 @@ void setup() {
   // read panel before we start to run
   //paramUpdate();
 
-  drum1.frequency(500);
-  drum2.frequency(500);
-  drum3.frequency(500);
-  drum4.frequency(500);
+#if 0
+  drum1.frequency(100);
+  drum2.frequency(66);
+  drum3.frequency(150);
+  drum4.frequency(223);
+#endif
 
   AudioNoInterrupts();
-  
+
   sgtl5000_1.setAddress(LOW);
   sgtl5000_1.enable();
-  //sgtl5000_1.inputSelect(myInput);
-  sgtl5000_1.volume(0.5);
+  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
+  sgtl5000_1.lineInLevel(15);
+  sgtl5000_1.volume(0.6);
+  sgtl5000_1.micGain(0); //- seems to influence noise on input?
+  sgtl5000_1.adcHighPassFilterDisable(); //- seems to influence noise on input?
+  sgtl5000_1.autoVolumeDisable();
+  //sgtl5000_1.audioProcessorDisable();
 
   // Enable the second audio shield, select input, and enable output
   sgtl5000_2.setAddress(HIGH);
   sgtl5000_2.enable();
-  //sgtl5000_2.inputSelect(myInput);
-  sgtl5000_2.volume(0.5);
+  sgtl5000_2.inputSelect(AUDIO_INPUT_LINEIN);
+  sgtl5000_2.lineInLevel(15);
+  sgtl5000_2.volume(0.6);
+  sgtl5000_2.micGain(0); //- seems to influence noise on input?
+  sgtl5000_2.adcHighPassFilterDisable(); //- seems to influence noise on input?
+  sgtl5000_2.autoVolumeDisable();
+  //sgtl5000_2.audioProcessorDisable();
 
+  
   AudioInterrupts();
 
 }
@@ -109,11 +107,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if(millis() == next)
+  if (millis() == next)
   {
     next = millis() + 1000;
 
-    trigger(count);
+    //trigger(count);
     count++;
 
     Serial.print("Diagnostics: max: ");
