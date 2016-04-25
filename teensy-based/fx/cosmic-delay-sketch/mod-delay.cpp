27a5dcd0e61fd32dc6190b858cec0e32d35df2c4
Jacquot-SFE <byron.jacquot@sparkfun.com>
2016-04-25 13:49:08 -0600

    Adding MIT license to delay FX files.

--- teensy-based/fx/cosmic-delay-sketch/mod-delay.cpp
+++ teensy-based/fx/cosmic-delay-sketch/mod-delay.cpp
@@ -1,4 +1,5 @@
-/*
+/* Modulated delay line
+ *  
  * Inspired by;
  * http://musicdsp.org/showArchiveComment.php?ArchiveID=154
  * 
@@ -6,9 +7,35 @@
  * No feedback within the class, just done externally
  * so we can add filters and stuff to the loop.
  * 
- * Delay time is an input to the block, and functions from 0 to 0x7fff,
- * scaling the block length accordingly.
+ * Delay time is a signal input to the block, and functions from 0 to 0x7fff,
+ * scaling the delay time accordingly.
  */
+/* 
+ * Copyright (c) 2016, Byron Jacquot, SparkFun Electronics
+ * SparkFun code, firmware, and software is released under 
+ * the MIT License(http://opensource.org/licenses/MIT).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice, development funding notice, and this permission
+ * notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * 
+ */
+ 
 
 #include "mod-delay.h"
 
--- teensy-based/fx/cosmic-delay-sketch/mod-delay.h
+++ teensy-based/fx/cosmic-delay-sketch/mod-delay.h
@@ -1,3 +1,40 @@
+/* Modulated delay line
+ *  
+ * Inspired by;
+ * http://musicdsp.org/showArchiveComment.php?ArchiveID=154
+ * 
+ * But details changed for Teensy Audio.
+ * No feedback within the class, just done externally
+ * so we can add filters and stuff to the loop.
+ * 
+ * Delay time is a signal input to the block, and functions from 0 to 0x7fff,
+ * scaling the delay time accordingly.
+ */
+/* 
+ * Copyright (c) 2016, Byron Jacquot, SparkFun Electronics
+ * SparkFun code, firmware, and software is released under 
+ * the MIT License(http://opensource.org/licenses/MIT).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice, development funding notice, and this permission
+ * notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * 
+ */
 
 #pragma once
  
@@ -38,7 +75,7 @@ public:
     Serial.print(' ');
     Serial.println(buffer_length, HEX);
     
-    };
+  };
   
 private:
 	audio_block_t *inputQueueArray[2];
